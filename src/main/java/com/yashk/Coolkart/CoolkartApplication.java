46f98fd69ce54ab54746f04e464be4da96ca5b2b
yash kolapkar <yashkolapkar7@gmail.com>
2025-03-18 23:56:59 +0530

    First Commit

---
+++ src/main/java/com/yashk/Coolkart/CoolkartApplication.java
@@ -0,0 +1,13 @@
+package com.yashk.Coolkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolkartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolkartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yashk/Coolkart/CoolkartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashk.Coolkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

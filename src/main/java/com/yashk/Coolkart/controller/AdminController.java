5a76a1062a29275cea5b6db9543f4016a6d9b473
ASHKING <73180810+YashKolapkar@users.noreply.github.com>
2025-04-08 00:37:31 +0530

    Frontend added for Admin add Product and category (#4)

--- src/main/java/com/yashk/Coolkart/controller/AdminController.java
+++ src/main/java/com/yashk/Coolkart/controller/AdminController.java
@@ -13,5 +13,15 @@ public class AdminController {
 
         return "/admin/index";
     }
+    @GetMapping("/loadAddProduct")
+    public String loadAddProduct(){
+
+        return "/admin/add_product";
+    }
+    @GetMapping("/category")
+    public String category(){
+
+        return "/admin/category";
+    }
 
 }
